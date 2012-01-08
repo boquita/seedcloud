610e803f278f03052aa094a309d1322214e33aa3
Iván Rodríguez <ceo@bocaboca.co>
2012-01-08 11:57:28 +0100

    Uploaded app.js (node + express + mongoose test)

---
+++ app.js
@@ -0,0 +1,34 @@
+var app = require('express').createServer();
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/mydb');
+var Schema = mongoose.Schema;
+ 
+var UserSchema = new Schema({
+  username : String,
+  title    : String
+});
+
+var userModel = mongoose.model('User', UserSchema);
+var user = new userModel();
+user.username = 'Ivan';
+user.title = 'Senior Developer';
+user.save(function(err) {
+  if (err) throw err;
+  else {
+    console.log('User saved, starting server...');
+    app.listen (8888);
+  }
+});  
+ 
+
+app.get('/find/:id', function(req, res) {
+   res.contentType('text/plain'); 
+    userModel.findOne({'username': req.params.id}, function(err, user) {
+      if (user != null) {
+        console.log('Found the User:' + user.username);
+        res.send(JSON.stringify(user));
+      }
+	else console.log ('no user found');
+    });
+});
+
